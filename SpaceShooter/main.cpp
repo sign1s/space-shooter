4afe9282e015b807dc1e4c8e88e7904d150a80e1
Laura Górna <lgorna7@gmail.com>
2026-01-12 19:29:30 +0100

    Dodaj pliki projektów.

---
+++ SpaceShooter/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+#include "Main_menu.h"
+#include "PauseMenu.h"
+#include "GameOverMenu.h"
+
+using namespace sf;
+
+
+
+int main()
+{
+	enum class State { MENU, PLAYING, SHOP, PAUSE, GAMEOVER };
+	State currentState = State::MENU;
+
+	RenderWindow window(VideoMode({ 1920,1080 }), "dragon shooter!");
+
+	//main menu
+	Main_menu Menu(window.getSize().x, window.getSize().y);
+
+	//pause menu
+	PauseMenu Pause(window.getSize().x, window.getSize().y);
+
+	//gameover menu
+	GameOverMenu OverScreen(window.getSize().x, window.getSize().y);
+
+	Game game(&window);
+
+	srand(time(NULL));
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+			/*if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+				window.close();*/
+			if (currentState == State::MENU) {
+
+				if (event.type == Event::KeyReleased) {
+
+					switch (event.key.code) {
+					case Keyboard::Up:
+						Menu.moveUp();
+						break;
+					case Keyboard::Down:
+						Menu.moveDown();
+						break;
+					case Keyboard::Enter:
+						int x = Menu.mainMenuPressed();
+						if (x == 0) {
+							game.Reset();
+							currentState = State::PLAYING;
+						}
+						if (x == 1) currentState = State::SHOP;
+						if (x == 2) window.close();
+						break;
+					}
+				}
+			}
+			else if (currentState == State::SHOP) {
+				if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+					currentState = State::MENU;
+			}
+			else if (currentState == State::PLAYING) {
+				if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
+					Pause.resetPause();
+					currentState = State::PAUSE;
+				}
+			}
+			else if (currentState == State::PAUSE) {
+				if (event.type == Event::KeyReleased) {
+					if (event.key.code == Keyboard::Up) Pause.moveUp();
+					if (event.key.code == Keyboard::Down) Pause.moveDown();
+					if (event.key.code == Keyboard::Enter) {
+						int x = Pause.pausePressed();
+						if (x == 0) currentState = State::PLAYING; // Wznów
+						if (x == 1) currentState = State::MENU; // WyjdŸ do menu						
+					}
+				}
+			}
+			else if (currentState == State::GAMEOVER) {
+				if (event.type == Event::KeyReleased) {
+					if (event.key.code == Keyboard::Up) OverScreen.moveUp();
+					if (event.key.code == Keyboard::Down) OverScreen.moveDown();
+					if (event.key.code == Keyboard::Enter) {
+						int x = OverScreen.buttonPressed();
+						if (x == 0) {
+							game.Reset();
+							currentState = State::PLAYING; // Wznów
+						}
+						if (x == 1) currentState = State::MENU; // WyjdŸ do menu						
+					}
+				}
+			}
+		}
+
+		//DRAWING
+		window.clear();
+
+		if (currentState == State::MENU) {
+			Menu.draw(window);
+		}
+		else if (currentState == State::SHOP) {
+			std::cout << "SHOP W TRAKCIE BUDOWY" << std::endl;
+			break;
+		}
+		else if (currentState == State::PLAYING) {
+			game.Update();
+			if (game.getGameOver()) {
+				currentState = State::GAMEOVER;
+			}
+			game.Draw();
+		}
+		else if (currentState == State::PAUSE) {
+			game.Draw();
+			Pause.draw(window);
+		}
+		else if (currentState == State::GAMEOVER) {
+			game.Draw();
+			OverScreen.draw(window);
+		}
+
+
+		window.display();
+	}
+}
